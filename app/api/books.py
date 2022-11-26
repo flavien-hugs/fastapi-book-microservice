c2f5c98d1dc4be92bbee05f7dcfba96068fb4b45
flavien-hugs <geekactux@gmail.com>
2022-11-26 03:03:01 +0000

    app/api/books.py

--- app/api/books.py
+++ app/api/books.py
@@ -1,32 +1,32 @@
 from typing import List
-from fastapi import Header, APIRouter
+from fastapi import Header, APIRouter, HTTPException
 
 from .models import Book
 
 
 fake_book_db = [
     {
-        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
-        'plot': 'The surviving members of the resistance face the First Order once again.',
-        'genres': ['Action', 'Adventure', 'Fantasy']
+        "name": "Star Wars: Episode IX - The Rise of Skywalker",
+        "plot": "The surviving members of the resistance face the First Order once again.",
+        "genres": ["Action", "Adventure", "Fantasy"],
     }
 ]
 
 books = APIRouter()
 
 
-@books.get('/')
+@books.get("/")
 async def indexPage():
     context = {"message": "Welcome to book microservice api"}
     return context
 
 
-@books.get('/books/', response_model=List[Book])
+@books.get("/books/", response_model=List[Book])
 async def list_book():
     return fake_book_db
 
 
-@books.post('/book/add/', status_code=201)
+@books.post("/book/add/", status_code=201)
 async def add_book(payload: Book):
     book = payload.dict()
     fake_book_db.append(book)
@@ -34,7 +34,7 @@ async def add_book(payload: Book):
     return context
 
 
-@books.put('/book/update/{id}/')
+@books.put("/book/update/{id}/")
 async def update_book(id: int, payload: Book):
     book = payload.dict()
     books_length = len(fake_book_db)
@@ -44,7 +44,7 @@ async def update_book(id: int, payload: Book):
     return HTTPException(status_code=404, detail="Book with given id not found")
 
 
-@books.put('/book/delete/{id}/')
+@books.put("/book/delete/{id}/")
 async def delete_book(id: int):
     books_length = len(fake_book_db)
     if 0 <= id <= books_length:
