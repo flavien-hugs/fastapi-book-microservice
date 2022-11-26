dfe05a0870349044f4d40bdaafb3629696151ddc
flavien-hugs <geekactux@gmail.com>
2022-11-26 04:46:01 +0000

    Edit app Book models

--- app/api/models.py
+++ app/api/models.py
@@ -1,8 +1,30 @@
-from typing import List
 from pydantic import BaseModel
+from typing import List, Optional
+from sqlalchemy import MetaData, Column, Integer, String, Table, ARRAY
+
+metadata = MetaData()
+
+books = Table(
+    'books',
+    metadata,
+    Column('id', Integer, primary_key=True, index=True),
+    Column('author_name', String(80)),
+    Column('book_description', String(250)),
+    Column('book_genres', ARRAY(String))
+)
 
 
-class Book(BaseModel):
-    name: str
-    plot: str
-    genres: List[str]
+class BookIn(BaseModel):
+    author_name: str
+    book_description: str
+    book_genres: List[str]
+
+
+class BookOut(BookIn):
+    id: int
+
+
+class BookUpdate(BookIn):
+    author_name: str
+    book_description: str
+    book_genres: List[str]
