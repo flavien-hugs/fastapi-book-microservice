eb99a66605090e85bca31dd57eb1723498b29175
flavien-hugs <geekactux@gmail.com>
2022-11-27 16:38:32 +0000

    Improve models

--- app/api/models.py
+++ app/api/models.py
@@ -4,20 +4,12 @@ from sqlalchemy import MetaData, Column, Integer, String, Table, ARRAY
 
 metadata = MetaData()
 
-books = Table(
-    "books",
-    metadata,
-    Column("id", Integer, primary_key=True, index=True),
-    Column("author_name", String(80)),
-    Column("book_description", String(250)),
-    Column("book_genres", ARRAY(String)),
-)
-
 
 class BookIn(BaseModel):
-    author_name: str
+    book_title: str
     book_description: str
     book_genres: List[str]
+    authors_id: List[int]
 
 
 class BookOut(BookIn):
@@ -25,6 +17,18 @@ class BookOut(BookIn):
 
 
 class BookUpdate(BookIn):
-    author_name: str
-    book_description: str
-    book_genres: List[str]
+    book_title: Optional[str] = None
+    book_description: Optional[str] = None
+    book_genres: Optional[List[str]] = None
+    authors_id: Optional[List[str]] = None
+
+
+books = Table(
+    "books",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("book_title", String(80)),
+    Column("book_description", String(250)),
+    Column("book_genres", ARRAY(String)),
+    Column("authors_id", ARRAY(Integer))
+)
